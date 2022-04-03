449b3704430abff3e76b5d6d694e2cdf021208bc
Riad Safowan <riad.safowan@northsouth.edu>
2022-04-03 16:48:32 +0600

    heroku deploy with pgsql

--- utils/const.go
+++ utils/const.go
@@ -1,4 +1,4 @@
 package utils
 
-// const BASEURL = "192.168.31.64:9090"
-const BASEURL = "https://quiet-garden-69930.herokuapp.com"
+const BASEURL = "192.168.31.64:9090"
+// const BASEURL = "https://quiet-garden-69930.herokuapp.com"
